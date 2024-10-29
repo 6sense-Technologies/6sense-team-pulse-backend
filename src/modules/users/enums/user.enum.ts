d43afe6f0e67cd52b8ebafa2ae53793915b309a7
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-10-29 15:47:02 +0600

    add new designations

--- src/modules/users/enums/user.enum.ts
+++ src/modules/users/enums/user.enum.ts
@@ -1,9 +1,14 @@
 export enum Designation {
   JuniorSoftwareEngineer = 'Jr. Software Engineer',
   SoftwareEngineerII = 'Software Engineer II',
-  SQAEngineerII = 'SQA Engineer II',
+  JuniorUIUXDesigner = 'Jr. UI/UX Designer',
   UIUXDesignerII = 'UI/UX Designer II',
   JrSQAEngineer = 'Jr. SQA Engineer',
+  SQAEngineerII = 'SQA Engineer II',
+  SqaIntern = 'SQA Intern',
+  BackendIntern = 'Backend Intern',
+  FrontendIntern = 'Frontend Intern',
+  UIUXIntern = 'UI/UX Intern',
 }
 
 export enum Project {
