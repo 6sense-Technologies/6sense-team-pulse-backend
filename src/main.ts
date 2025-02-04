ae03c59e2776a345e46eec527e0fd32e7a940c77
ahmedfaiyaz-6sense <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2025-02-04 13:55:11 +0600

    allowed all origin to access api

--- src/main.ts
+++ src/main.ts
@@ -24,15 +24,23 @@ async function bootstrap(): Promise<void> {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+   
   // app.enableCors({
   //   origin: '*', // Allows any origin
   // });
   app.useGlobalPipes(new ValidationPipe());
+  
+  // Enable CORS for all origins
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://6sense-efficiency.vercel.app','https://o4t-under-development.vercel.app'],
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
+    origin: '*',  // Allows all origins to access the API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Allows these HTTP methods
+    allowedHeaders: 'Content-Type, Accept',  // Allowed headers
   });
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://6sense-efficiency.vercel.app','https://o4t-under-development.vercel.app'],
+  //   methods: 'GET,POST,PUT,DELETE',
+  //   credentials: true,
+  // });
   const configService = app.get(ConfigService);
   const mqttMicroservice =
     await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
